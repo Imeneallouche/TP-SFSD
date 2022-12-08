96462d37d2fa19f6dd02992eb09ccd9b70c9e654
Imène ALLOUCJE <https://github.com/Imeneallouche>
2022-12-08 17:32:04 +0100

    first version fo recherche has been added

--- VERSION FINALE/lib.c
+++ VERSION FINALE/lib.c
@@ -856,6 +856,80 @@ void Chargement_initial_TOVnC(char nom_fichier[], int n)
     Fermer_TOVnC(F);
 }
 
+/****************************************************|
+|                                                    |
+|   Recherche dun materiel selon son identifiant     |
+|                                                    |
+|****************************************************/
+void Recheche_TOVnC(char nom_fichier[], char Identifiant_Recherche[], int *trouv, int *i, int *j)
+{
+    fichier_TOVnC f;
+    Ouvrir_TOVnC(&f, nom_fichier, 'A');
+    int binf = 1,                                // le premier bloc
+        bsup = Entete_TOVnC(&f, 1);              // le dernier bloc
+    char Cle_Min[TAILLE_IDENTIFIANT + 1],        // la plus petite cle dans un bloc (plus petit identifiant)
+        Cle_Max[TAILLE_IDENTIFIANT + 1],         // la plus grande cle dans un bloc (plus grand identifiant)
+        Cle_Courrante[TAILLE_IDENTIFIANT + 1],   // la cle courrant dont on s'est arrete dans le parcours
+        Identifiant[TAILLE_IDENTIFIANT + 1],     // numero d'identifiant(cle)
+        Supprime[TAILLE_SUPPRIMER + 1],          // supprimer='f' l'element n'a pas ete supprime supprimer='t' sinon
+        Materiel[TAILLE_MATERIEL + 1],           // le type du materiel
+        Fonctionne[TAILLE_FONCTIONNEMENT + 1],   // fonctionne = 'f', le materiel marche, fonctionne = 'n' sinon
+        Prix[TAILLE_PRIX + 1],                   // le ptix du materiel
+        Taille[TAILLE_TAILLE + 1],               // taille du champs description
+        Description[TAILLE_MAX_DESCRIPTION + 1]; // la description (caracteristiques) du materiel
+    Tbloc_TOVnC Buf;                             // un buffer pour charger un bloc de MS vers MC
+    while (!(*trouv) && binf <= bsup)            // enregistrement non trouvé et recherche possible
+    {
+        *i = (binf + bsup) / 2;                                                   // numero de bloc a parcourir
+        *j = 0;                                                                   // la premiere position dans le bloc
+        LireDir_TOVnC(&f, *i, &Buf);                                              // lire le buffer
+        extraire_chaine_TOVnC(Cle_Min, j, TAILLE_IDENTIFIANT, &Buf);              // extraire la plus petite cle (premiere cle) de du bloc i
+        strcat(Cle_Courrante, Cle_Min);                                           // mettre a jour la cle courrante
+        strcat(Cle_Max, Buf.cleMax);                                              // lire la cle max qui est dans le tableau
+        if (Identifiant_Recherche >= Cle_Min && Identifiant_Recherche <= Cle_Max) // si la cle à recherchee est entre Cle_Min et Cle_Max du bloc on fait le recherche sequentielle dans le bloc
+        {
+            while (!(*trouv) && *j < Buf.nb)
+            {
+                if (Fonctionne == 'f')
+                    printf("Fonctionne? : OUI\n");
+                extraire_chaine_TOVnC(&Supprime, j, TAILLE_SUPPRIMER, &Buf); // récupérer un caractère d'Supprimeacement
+                if (Identifiant_Recherche == Cle_Courrante && !Supprime)     // la cle st donc trouveée dans le bloc i
+                {
+                    extraire_chaine_TOVnC(Materiel, j, TAILLE_MATERIEL, &Buf);
+                    extraire_chaine_TOVnC(Fonctionne, j, TAILLE_FONCTIONNEMENT, &Buf);
+                    extraire_chaine_TOVnC(Prix, j, TAILLE_PRIX, &Buf);
+                    extraire_chaine_TOVnC(Taille, j, TAILLE_TAILLE, &Buf);
+                    extraire_chaine_TOVnC(Description, j, atoi(Taille), &Buf);
+                    *trouv = 1;
+                    printf("\n L'enregistrement est dans le bloc %d a la position %d", i, j);
+                    printf("\n Les informations du materiel:  ----------------------------------------\n\n");
+                    printf(" -> L'identifiant : %s\n", Identifiant);
+                    printf(" -> Le type matériel : %s\n", Materiel);
+                    printf(" -> Fonctionnement du materiel: %c\n", Fonctionne);
+                    printf(" -> Le prix d'achat du matériel : %s\n", Prix);
+                    printf(" -> La Description : %s\n", Description);
+                }
+                else
+                {
+                    *j = *j + TAILLE_MATERIEL + TAILLE_FONCTIONNEMENT + TAILLE_PRIX;
+                    extraire_chaine_TOVnC(Taille, j, TAILLE_TAILLE, &Buf); // récupérer un tableau de 3 caractères, la taille d'enregistrement
+                    *j = *j + atoi(Taille);
+                    extraire_chaine_TOVnC(Cle_Courrante, j, TAILLE_IDENTIFIANT, &Buf);
+                }
+            }
+        }
+        else
+        {
+            if (Identifiant_Recherche < Cle_Min) // la clé n'est pas dans ce bloc
+                bsup = *i - 1;                   // la clé doit être avant le bloc courant
+            else
+                binf = *i + 1; /// clé  doit être après le bloc courant
+        }
+    }
+    if (!(*trouv))
+        printf("\nEnregistrement n'existe pas!\n");
+}
+
 /*
 
 
